d93ffc5b409dba1cc64817ef0dc1c20d6a0ca980
Jeffrey Hann <jeffreyhann@Jeffreys-MacBook-Pro.local>
2017-06-11 14:39:58 -0300

    dropping view

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,6 @@ import Errors
 dispatch :: [(String, [String] -> IO ())]
 dispatch =  [ ("load", S3.load)
             , ("save", S3.save)
-            , ("view", view)
             , ("help", help)
             , ("version", version)
             ]
@@ -25,13 +24,6 @@ help [] = putStrLn "Usage: donair [add|load|remove|help|version]"
 
 version :: [String] -> IO ()
 version [] = putStrLn "Donair Safe 0.0.1"
-
-view :: [String] -> IO ()
-view [fileName] = do
-    contents <- readFile fileName
-    putStr contents
-view [] = do
-    Errors.die "Please provide a filename"
 
 exit :: IO ()
 exit = exitWith ExitSuccess
