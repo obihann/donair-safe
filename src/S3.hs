de8a4e18a4b28a6fdbaa750f5852ff771885d9d6
Jeffrey Hann <jeffreyhann@Jeffreys-MacBook-Pro.local>
2017-06-13 21:44:03 -0300

    hey now we can upload

--- src/S3.hs
+++ src/S3.hs
@@ -1,19 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-{-# LANGUAGE ScopedTypeVariables #-}-}
 
 module S3 where
 
-import qualified Aws
-import qualified Aws.S3 as S3
-import           Data.Conduit (($$+-))
-import           Data.Conduit.Binary (sinkFile)
-import           Network.HTTP.Conduit (withManager, responseBody, RequestBody(..))
-import Data.Text (pack)
+{-import qualified Aws-}
+{-import qualified Aws.S3 as S3-}
+{-import           Data.Conduit (($$+-))-}
+{-import           Data.Conduit.Binary (sinkFile)-}
+{-import           Network.HTTP.Conduit (withManager, responseBody, RequestBody(..))-}
 import Control.Monad.IO.Class
-import qualified Data.Text as T
+{-import qualified Data.Text as T-}
 import qualified Data.ByteString.Lazy as L
-import qualified Data.ByteString as S
-import System.Posix.Files
+{-import qualified Data.ByteString as S-}
+{-import System.Posix.Files-}
+
+import Data.Text (pack)
+import Control.Lens
+import Network.AWS
+import Network.AWS.S3
 import System.IO
+
 import Errors
 
 bucket = "donair-safe"
@@ -22,42 +28,57 @@ bucket = "donair-safe"
 {-getConfig = (Aws.baseConfiguration, Aws.defServiceConfig)-}
 
 encrypt :: [String] -> IO ()
-encrypt [fileName, key] = do
-    putStrLn fileName
-    putStrLn key
+{-encrypt [fileName, key] = do-}
+    {-putStrLn fileName-}
+    {-putStrLn key-}
 encrypt [] = Errors.die "Please provide a filename to encrypt"
 encrypt [fileName] = Errors.die "Please provide an encryption key"
 
 load :: [String] -> IO ()
-load [fileName] = do
-    cfg <- Aws.baseConfiguration
-    let s3cfg = Aws.defServiceConfig :: S3.S3Configuration Aws.NormalQuery
+{-load [fileName] = do-}
+    {-cfg <- Aws.baseConfiguration-}
+    {-let s3cfg = Aws.defServiceConfig :: S3.S3Configuration Aws.NormalQuery-}
 
-    withManager $ \mgr -> do
-        S3.GetObjectResponse { S3.gorResponse = rsp } <-
-            Aws.pureAws cfg s3cfg mgr $
-                S3.getObject bucket (pack fileName)
+    {-withManager $ \mgr -> do-}
+        {-S3.GetObjectResponse { S3.gorResponse = rsp } <--}
+            {-Aws.pureAws cfg s3cfg mgr $-}
+                {-S3.getObject bucket (pack fileName)-}
 
-        responseBody rsp $$+- sinkFile fileName
+        {-responseBody rsp $$+- sinkFile fileName-}
 load [] = Errors.die "Please provide a filename to load"
+
+{-save :: [String] -> IO ()-}
+{-save [fileName] = do-}
+    {-cfg <- Aws.baseConfiguration-}
+    {-let s3cfg = Aws.defServiceConfig :: S3.S3Configuration Aws.NormalQuery-}
+
+    {-withManager $ \mgr -> do-}
+        {-let streamer sink = withFile fileName ReadMode $ \h -> sink $ S.hGet h 10240-}
+        {-b <- liftIO $ L.readFile fileName-}
+        {-size <- liftIO $ (fromIntegral . fileSize <$> getFileStatus fileName :: IO Integer)-}
+        {-let body = RequestBodyStream (fromInteger size) streamer-}
+        {-rsp <- Aws.pureAws cfg s3cfg mgr $-}
+            {-(S3.putObject bucket (T.pack fileName) body)-}
+                {-{ S3.poMetadata =-}
+                    {-[ ("mediatype", "texts")-}
+                    {-, ("meta-description", "test Internet Archive item made via haskell aws library")-}
+                    {-]-}
+                {-, S3.poAutoMakeBucket = True-}
+                {-}-}
+        {-liftIO $ print rsp-}
 
 save :: [String] -> IO ()
 save [fileName] = do
-    cfg <- Aws.baseConfiguration
-    let s3cfg = Aws.defServiceConfig :: S3.S3Configuration Aws.NormalQuery
+    env <- newEnv Discover
+    log <- newLogger Debug stdout
+    body <- toBody <$> hashedFile fileName
 
-    withManager $ \mgr -> do
-        let streamer sink = withFile fileName ReadMode $ \h -> sink $ S.hGet h 10240
-        b <- liftIO $ L.readFile fileName
-        size <- liftIO $ (fromIntegral . fileSize <$> getFileStatus fileName :: IO Integer)
-        let body = RequestBodyStream (fromInteger size) streamer
-        rsp <- Aws.pureAws cfg s3cfg mgr $
-            (S3.putObject bucket (T.pack fileName) body)
-                { S3.poMetadata =
-                    [ ("mediatype", "texts")
-                    , ("meta-description", "test Internet Archive item made via haskell aws library")
-                    ]
-                , S3.poAutoMakeBucket = True
-                }
-        liftIO $ print rsp
+    let bucketName = BucketName "donair-safe"
+    let objectKey = ObjectKey $ pack fileName
+
+    runResourceT . runAWS (env & envLogger .~ log) $
+        send (putObject bucketName objectKey body)
+
+    putStrLn "done"
+
 save [] = Errors.die "Please provide a filename to load"
